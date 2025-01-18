f494b4700910b5419f86be74c63bfe631bcadc44
Sebastian Hartte <shartte@users.noreply.github.com>
2025-01-18 21:41:44 +0100

    Initial Docusaurus Content

---
+++ docs/docs/intro.md
@@ -0,0 +1,7 @@
+---
+sidebar_position: 1
+slug: /
+---
+
+See [Authoring Pages](authoring/index.md) if you are interested in learning how to create content for a GuideMD
+guidebook.
---
+++ docs/docusaurus.config.ts
@@ -0,0 +1,140 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+  title: 'GuideME Documentation',
+  tagline: 'Cool Guides',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://guideme.appliedenergistics.org',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  organizationName: 'AppliedEnergistics',
+  projectName: 'GuideME',
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          editUrl: 'https://github.com/AppliedEnergistics/GuideME/tree/main/docs/',
+          routeBasePath: '/', // Serve the docs at the site's root
+        },
+        blog: false,
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: 'GuideME',
+      logo: {
+        alt: 'GuideME Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'docsSidebar',
+          position: 'left',
+          label: 'Documentation',
+        },
+        {
+          href: 'https://github.com/AppliedEnergistics/GuideME',
+          label: 'GitHub',
+          position: 'left',
+        },
+        {
+          href: 'https://curseforge.com/minecraft/mc-mods/guideme',
+          label: 'CurseForge',
+          position: 'left',
+        },
+        {
+          href: 'https://modrinth.com/mod/guideme',
+          label: 'Modrinth',
+          position: 'left',
+        },
+        {
+          href: 'https://github.com/AppliedEnergistics/GuideME',
+          label: 'Maven',
+          position: 'left',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Tutorial',
+              to: '/docs/intro',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/docusaurus',
+            },
+            {
+              label: 'X',
+              href: 'https://x.com/docusaurus',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: '/blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/facebook/docusaurus',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
