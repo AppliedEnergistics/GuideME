8683932fbf88f4773eaad68e81f2f17f01908240
Sebastian Hartte <shartte@users.noreply.github.com>
2025-06-21 13:11:51 +0200

    Add support for block annotation templates.
    (cherry picked from commit ae6fe5e955ded8f8611d2ecd5c5a9c82613acac5)

--- src/main/java/guideme/scene/annotation/DiamondAnnotationElementCompiler.java
+++ src/main/java/guideme/scene/annotation/DiamondAnnotationElementCompiler.java
@@ -5,7 +5,9 @@ import guideme.compiler.PageCompiler;
 import guideme.compiler.tags.MdxAttrs;
 import guideme.document.LytErrorSink;
 import guideme.libs.mdast.mdx.model.MdxJsxElementFields;
+import guideme.scene.GuidebookScene;
 import java.util.Set;
+import net.minecraft.core.BlockPos;
 import org.jetbrains.annotations.Nullable;
 import org.joml.Vector3f;
 
@@ -18,8 +20,16 @@ public class DiamondAnnotationElementCompiler extends AnnotationTagCompiler {
     @Override
     protected @Nullable SceneAnnotation createAnnotation(PageCompiler compiler, LytErrorSink errorSink,
             MdxJsxElementFields el) {
+        return createAnnotation(null, compiler, errorSink, el, BlockPos.ZERO);
+    }
+
+    @Override
+    protected @Nullable SceneAnnotation createAnnotation(GuidebookScene scene, PageCompiler compiler,
+            LytErrorSink errorSink, MdxJsxElementFields el, BlockPos instancePosition) {
         var pos = MdxAttrs.getVector3(compiler, errorSink, el, "pos", new Vector3f());
         var color = MdxAttrs.getColor(compiler, errorSink, el, "color", ConstantColor.WHITE);
+
+        pos.add(instancePosition.getX(), instancePosition.getY(), instancePosition.getZ());
 
         return new DiamondAnnotation(pos, color);
     }
