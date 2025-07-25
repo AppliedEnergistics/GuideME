4155005ff01292c2d918aaca41dfb198e22e91bf
Sebastian Hartte <shartte@users.noreply.github.com>
2025-07-26 01:08:41 +0200

    Fix Lucene startup error log about the incubating JDK vector feature.
    (cherry picked from commit ce8879aafb3ba151f24b78fb8d73e1434c9af69a)

---
+++ src/main/java/guideme/internal/hooks/mixins/LuceneVectorizationMixin.java
@@ -0,0 +1,19 @@
+package guideme.internal.hooks.mixins;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+/**
+ * This mixin forces Lucene not to rely on the incubating Vector module, which currently causes it to print a startup
+ * error to console, and, when someone *does* enable the incubating module, crashes due to us not shipping the
+ * corresponding provider.
+ */
+@Mixin(targets = "guideme.internal.shaded.lucene.internal.vectorization.VectorizationProvider", remap = false)
+public class LuceneVectorizationMixin {
+    @Inject(method = "getUpperJavaFeatureVersion", cancellable = true, at = @At("TAIL"))
+    private static void getUpperJavaFeatureVersion(CallbackInfoReturnable<Integer> ci) {
+        ci.setReturnValue(17);
+    }
+}
